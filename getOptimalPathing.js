821b96d5c2e3041e2336cbb7d923e956c006fee6
BuildTools <unconfigured@null.spigotmc.org>
2023-06-04 23:58:15 +1000

    Update algorithm with new changes

--- getOptimalPathing.js
+++ getOptimalPathing.js
@@ -1,6 +1,13 @@
 // Function to merge two cars
-function mergeCars(car1, car2) {
-    // Your implementation to merge two cars goes here
+const mergeCars = (cars, personA, personB) => {
+    const car1People = cars.find((car) => car.includes(personA))
+    const car2People = cars.find((car) => car.includes(personB))
+
+    const filteredOutCar1AndCar2 = cars.filter((car) => {
+        return !car.includes(personA) || !car.includes(personB)
+    })
+
+    return [...filteredOutCar1AndCar2, [...car1People, ...car2People]]
 }
 
 const calculateDetour = (car1, car2, graph) => {
@@ -12,7 +19,7 @@ const calculateDetour = (car1, car2, graph) => {
 // Main function to solve the carpooling problem
 export const solveCarpooling = (distanceMatrix, detourThreshold) => {
     const graph = []
-    let cars = []
+    const cars = []
 
     // Builds a complete graph using the Google Maps Distance Matrix
     for (let i = 0; i < distanceMatrix.destination_addresses.length; i++) {
@@ -23,46 +30,21 @@ export const solveCarpooling = (distanceMatrix, detourThreshold) => {
     }
 
     // Step 2: Initial solution (each friend in a separate car)
-    for (let i = 0; i < distanceMatrix.destination_addresses.length; i++) {
-        cars.push([friends[i]])
+    for (let i = 0; i < distanceMatrix.destination_addresses.length - 1; i++) {
+        cars.push([i])
     }
 
-    let solutionImproved = true
-    while (solutionImproved) {
-        solutionImproved = false
-
-        // Step 3: Merge cars with low detour
-        for (let i = 0; i < cars.length - 1; i++) {
-            for (let j = i + 1; j < cars.length; j++) {
-                const detour = calculateDetour(cars[i], cars[j], graph)
-                if (detour <= detourThreshold) {
-                    cars = mergeCars(cars[i], cars[j])
-                    cars.splice(j, 1) // Remove the merged car
-                    solutionImproved = true
-                    break
-                }
-            }
-            if (solutionImproved) {
+    // Step 3: Merge cars with low detour
+    for (let i = 1; i < cars.length - 1; i++) {
+        for (let j = i + 1; j < cars.length - 1; j++) {
+            const detour = calculateDetour(cars[i], cars[j], graph)
+            console.log(`Car 1: ${i}, Car 2: ${j}, Detour distance: ${detour}`)
+            if (detour <= detourThreshold) {
+                cars = mergeCars(cars, i, j)
                 break
             }
         }
-
-        // Step 4: Apply local search (e.g., 2-opt) to improve solution
-        // Your implementation of the local search algorithm goes here
     }
 
-    // Step 6: Return the final solution
     return cars
 }
-
-// Example usage
-const friends = [
-    { name: 'Friend1', address: 'Address1' },
-    { name: 'Friend2', address: 'Address2' },
-    // Add more friends with their addresses
-]
-
-const detourThreshold = 15 // Set an appropriate threshold
-
-const carpoolSolution = solveCarpooling(friends, detourThreshold)
-console.log(carpoolSolution)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { Client } from '@googlemaps/google-maps-services-js'
 import { addresses } from './address.js'
+import { solveCarpooling } from './getOptimalPathing.js'
 
 // Initialize the Google Maps client
 const googleMapsClient = new Client()
@@ -15,6 +16,25 @@ const app = express()
 
 // Define routes
 app.get('/', (req, res) => {
+    googleMapsClient
+        .distancematrix({
+            params: {
+                origins: locations,
+                destinations: locations,
+                key: 'AIzaSyBrSQ0XlKidDeg2nQUIk3nvn25R9XZaWqw',
+            },
+            timeout: 1000, // Timeout in milliseconds (optional)
+        })
+        .then((response) => {
+            // Handle the API response
+            res.send(solveCarpooling(response.data, 1000))
+        })
+        .catch((err) => {
+            console.error('Error making API request:', err)
+        })
+})
+
+app.get('/raw', (req, res) => {
     googleMapsClient
         .distancematrix({
             params: {
